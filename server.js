5492e57ebf972da642a2a1aa6e7fe2f4c0045747
George Yue <george.yue@ticketmaster.com>
2015-03-14 12:42:36 -0700

    update master

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Hapi = require('hapi');
 
 // Create a server with a host and port
 var server = new Hapi.Server();
-var http_port = process.env.PORT || 1337;
+var http_port = process.env.PORT || 5000;
 
 server.connection({
     host: 'localhost',
